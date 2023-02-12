804ccf338d64518b2e49f690ca58ac91510aff37
amir-mohammad <amirm.azarbashi@gmail.com>
2023-02-12 04:43:08 +0330

    isMultiplied?

--- e-24/main.cpp
+++ e-24/main.cpp
@@ -8,13 +8,14 @@ using namespace std;
 
 int main()
 {
-    int a, b, max;
+    int a, b;
 
     cout << "Input first number: ";
-    cin >> a;
+    cin >> a; // get first number and store in a
     cout << "Input second number: ";
-    cin >> b;
+    cin >> b; // get second number and store in b
 
+    // if a less than b you should change value of them
     if (a < b)
     {
         int temp;
@@ -23,13 +24,14 @@ int main()
         b = temp;
     }
 
+    // check a and b are multiplied?
     if (a % b == 0)
     {
-        cout << "Multiplied!" << endl;
+        cout << "Multiplied!" << endl; // print multiplied
     }
     else
     {
-        cout << "Not Multiplied!" << endl;
+        cout << "Not Multiplied!" << endl; // print not multiplied
     }
 
     return 0;
